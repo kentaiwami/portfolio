6393d02b9ecb6a36995e13cb1ec656c33b501dc6
Kenta Iwami <b1013068@fun.ac.jp>
2017-02-22 02:18:38 +0900

    簡単なviewの作成，モデルの作成

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Identifier(models.Model):
+    """
+    Model of engineer or photographer identifiers
+    :param name: Name of a identifier
+    :type name: str
+    """
+
+    name = models.CharField(max_length=20)
+
+
+class Product(models.Model):
+    """
+    Model of engineer and photographer works
+    :param product_name: Name of a product
+    :param votes: Good counts to a product
+    :param identifier: Identifier ForeignKey
+    :type product_name: str
+    :type votes: int
+    :type identifier: int
+    """
+
+    product_name = models.CharField(max_length=50)
+    votes = models.IntegerField(default=0)
+    identifier = models.ForeignKey(Identifier, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    """
+    Model of Comments to products
+    :param comment_text: Text of comment to products
+    :param pub_date: publish date of comment
+    :param product: Product ForeignKey
+    :type comment_text: str
+    :type pub_date: date
+    :type product: int
+    """
+
+    comment_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('date published')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
