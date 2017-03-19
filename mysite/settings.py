65204cad9db707fc7f4738dbaf91914799719754
Kenta Iwami <b1013068@fun.ac.jp>
2017-03-19 17:51:59 +0900

    DEBUG modeに戻した

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+flbcdz$c%jf-=r+%bde=*zq*pg#qoc&5c%17=bs351@ks%tc+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
