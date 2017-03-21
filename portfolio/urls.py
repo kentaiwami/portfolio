9ffc2facc27a53cdcf8c44a812fd38d4b17e0181
Kenta Iwami <b1013068@fun.ac.jp>
2017-03-21 16:09:45 +0900

    enjineer_works_allをコメントアウトした

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,7 @@ app_name = 'portfolio'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<product_id>[0-9]+)/$', views.engineer_work_detail, name='engineer_work_detail'),
-    url(r'^engineer_works_all', views.engineer_works_all, name='engineer_works_all'),
+    # url(r'^engineer_works_all', views.engineer_works_all, name='engineer_works_all'),
 ]
 handler404 = 'views.handler404'
 handler500 = 'views.handler500'
