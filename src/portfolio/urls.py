b7a88c8f2fbd0009a51bdaaa0e6c3aa9d467d7b7
Kenta Iwami <kentaiwami12@gmail.com>
2020-02-02 00:43:14 +0900

    コメントアウトを削除

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -14,7 +14,3 @@ urlpatterns = [
     url(r'^health_check/', views.health_check, name='health_check'),
     # url(r'^engineer_works_all', views.engineer_works_all, name='engineer_works_all'),
 ]
-# handler404 = 'views.handler404'
-# handler500 = 'views.handler500'
-# handler403 = 'views.handler403'
-# handler400 = 'views.handler400'
